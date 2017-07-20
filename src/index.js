d646ec72e3fa38159cb8ebcc661e6b8e37dd2935
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2017-07-20 12:14:45 +0300

    best solution

--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
 /* eslint-disable one-var, one-var-declaration-per-line, no-plusplus, no-console */
-function getRowLength(s = '', n = 3) {
-  const a = s.split(''), b = [], l = a.length;
-  for (let i = 0; i < n && i < l; i += 1) b[i] = [];
-  while (a.length > 0) {
-    for (let i = 0; i < n && a.length > 0; i += 1) b[i].push(a.shift());
-    for (let i = n - 2; i > 0 && a.length > 0; i -= 1) b[i].push(a.shift());
-  }
-  return b.map(v => v.length);
+function encodeRailFenceCipher(string, numberRails) {
+  const symbols = string.split('');
+  const period = 2 * (numberRails - 1);
+  const rails = symbols.reduce((acc, symbol, index) => {
+    for (let i = 0; i < numberRails; i++) {
+      if ((index - i) % period === 0 || (index + i) % period === 0) {
+        acc[i] = (acc[i] || []).concat(symbol);
+      }
+    }
+    return acc;
+  }, []);
+  return rails.reduce((acc, el) => acc.concat(el), []).join('');
 }
 
-function encodeRailFenceCipher(s = '', n = 3) {
-  const a = s.split(''), b = [], l = a.length;
-  for (let i = 0; i < n && i < l; i += 1) b[i] = [];
-  while (a.length > 0) {
-    for (let i = 0; i < n && a.length > 0; i += 1) b[i].push(a.shift());
-    for (let i = n - 2; i > 0 && a.length > 0; i -= 1) b[i].push(a.shift());
+function decodeRailFenceCipher(string, numberRails) {
+  const symbols = string.split('');
+  const period = 2 * (numberRails - 1);
+  const len = Math.floor(symbols.length / period) + 1;
+  const res = [];
+  let pos = 0;
+  for (let i = 0; i < numberRails; i++) {
+    for (let j = 0; j <= len; j++) {
+      const el1 = i + (j * period);
+      const el2 = ((j + 1) * period) - i;
+      if (!res[el1] && el1 < symbols.length) res[el1] = symbols[pos++];
+      if (!res[el2] && el2 < symbols.length) res[el2] = symbols[pos++];
+    }
   }
-  return b.map(v => v.join('')).join('');
-}
-
-function decodeRailFenceCipher(s = '', n = 3) {
-  if (!s) return '';
-  const a = getRowLength(s, n), b = [];
-  for (let i = 0, sum = 0, l = a.length; i < l; i += 1) {
-    const rowLength = a[i];
-    a[i] = s.substr(sum, rowLength).split('');
-    sum += rowLength;
-  }
-  for (let j = 0, l = s.length; j <= l; j += (n + (n - 2))) {
-    for (let i = 0; i < n; i += 1) b.push(a[i].shift());
-    for (let i = n - 2; i > 0; i -= 1) b.push(a[i].shift());
-  }
-  return b.join('');
+  return res.join('');
 }
 
 export {
